37e0b8ab267226c4224190f81548f630f94ab947
dawit <dawitdb@yahoo.com>
2013-06-17 17:36:23 +0300

    New Version of Ethiopian Calender
    I have once again maintained the code to work with MVC4 and not to load
    the script again on the clients page

---
+++ EthCal/AmharicCalender.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web;
+
+
+
+namespace EthCal
+{
+    public static class AmharicCalender
+    {
+        public static MvcHtmlString AmCal(this HtmlHelper helper, string id)
+        {
+
+
+            string amid = id + "_am";
+            var scrips = string.Format(@"<link href='/Scripts/js/humanity.calendars.picker.css' rel='stylesheet' type='text/css'/>
+                                 <script type='text/javascript' src='/Scripts/2012.1.419/telerik.calendar.min.js')'></script>
+                                 <script type='text/javascript' src='/Scripts/2012.1.419/telerik.datepicker.min.js')'> </script>
+                                 <script type='text/javascript' src='/Scripts/js/jquery.calendars.js')'> </script>
+                                 <script type='text/javascript' src='/Scripts/js/jquery.calendars.js')'> </script>
+                                 <script type='text/javascript' src='/Scripts/js/jquery.calendars.plus.js')'> </script>
+                                 <script type='text/javascript' src='/Scripts/js/jquery.calendars.picker.js')'> </script>
+                                 <script type='text/javascript' src='/Scripts/js/jquery.calendars.ethiopian.js')'> </script>
+                                 <script type='text/javascript' src='/Scripts/js/jquery.calendars.picker-am.js')'> </script>
+                                 <script type='text/javascript' src='/Scripts/js/jquery.calendars.ethiopian-am.js')'> </script>");
+
+
+            string headerFiles = string.Format(@"<input id='{0}' class='calendarspicker t-input' type='input' data-gregorian-field='input[name={1}]' />", amid, id);
+
+
+
+
+
+            var fun = @"<script type='text/javascript'>
+
+             $(function() {
+
+                 $('.calendarspicker').calendarsPicker({
+                     calendar: $.calendars.instance('ethiopian', 'am')
+                     //disableInput: true
+                 }).attr('readonly', 'readonly').attr('style', 'background-color : #fff');
+
+                 $.calendars.picker.setDefaults({ dateFormat: 'dd-MMM-yyyy' });
+                 //set the receipt hidden date using the telrik date picker control
+                 var eCalendar = $.calendars.instance('ethiopian', 'am');
+                 var valueToset = new Date();
+                 if ($('#@id').data('tDatePicker').value() != null) {
+                     valueToset = $('#@id').data('tDatePicker').value();
+                 }
+                 var jstartdate = eCalendar.fromJSDate(valueToset).toJD();
+                 var edate = eCalendar.fromJD(jstartdate);
+                 //set the Amharic respective
+                 $('#@amid').val(edate.formatDate('dd-MMM-yyyy'));
+                 $('#@id').val($.datepicker.formatDate('dd-M-yy', valueToset));
+             });
+         </script>";
+
+
+            HttpContextBase context = helper.ViewContext.HttpContext;
+            // don't add the file if it's already there
+            if (context.Items.Contains(scrips) && context.Items.Contains(fun))
+                return MvcHtmlString.Create(headerFiles);
+            else
+            {
+                context.Items.Add(scrips, scrips);
+                context.Items.Add(fun, fun);
+            }
+            return MvcHtmlString.Create(scrips + headerFiles + fun);
+        }
+
+
+    }
+
+
+}
--- EthCal/Properties/AssemblyInfo.cs
+++ EthCal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ETH_Calender")]
+[assembly: AssemblyTitle("EthCal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ETH_Calender")]
+[assembly: AssemblyProduct("EthCal")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ef90d0ed-ce65-4846-aff2-59f95a7a8a69")]
+[assembly: Guid("6c594930-0b53-48a8-9ffb-fb653165fcc6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
